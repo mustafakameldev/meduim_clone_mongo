1ce60e03bffe6c5420c945b52022933bda411755
Mustafa Kamel <37935806+mustafakameldev@users.noreply.github.com>
2023-03-14 10:18:40 +0200

    create admin

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,12 +1,15 @@
 import { Injectable, HttpException, HttpStatus } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-import { hashPassword } from 'src/utils/ecrypt';
+import { comparePassword, hashPassword } from 'src/utils/ecrypt';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { User, UserDocument } from './schemas/user.schema';
 import { sign } from 'jsonwebtoken';
 import { UserResponseInterface } from './types/userResponse.interface';
 import { JWT_SECRET } from 'src/utils/config';
+import { CreateAdminDto } from './dtos/create-admin.dto';
+import { UserRole } from './interfaces/role-tyoe.emun';
+import { LoginDto } from './dtos/login.dto';
 @Injectable()
 export class UserService {
   constructor(
@@ -33,7 +36,9 @@ export class UserService {
     newUser.password = await hashPassword(newUser.password);
     newUser.createdAt = new Date();
     newUser.updatedAt = new Date();
-    return await this.userModel.create(newUser);
+    const user = await this.userModel.create(newUser);
+    delete user['password'];
+    return user;
   }
 
   buildUserResponse(user: User): UserResponseInterface {
@@ -66,5 +71,56 @@ export class UserService {
     // } catch (error) {
     //   console.log('sdfsf', error);
     // }
+  }
+
+  async createAdmin(userDto: CreateAdminDto): Promise<User> {
+    const findByUser = await this.userModel.findOne({
+      username: userDto.username,
+    });
+    if (findByUser) {
+      throw new HttpException(
+        'Username is used',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    const findByEmail = await this.userModel.findOne({
+      email: userDto.email,
+    });
+    if (findByEmail) {
+      throw new HttpException('email is used', HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+    const newUser = new User();
+    Object.assign(newUser, userDto);
+    newUser.password = await hashPassword(newUser.password);
+    newUser.createdAt = new Date();
+    newUser.updatedAt = new Date();
+    newUser.role = UserRole.admin;
+    const user = await this.userModel.create(newUser);
+    delete user['password'];
+    return user;
+  }
+  async login(loginDto: LoginDto): Promise<User> {
+    const user =
+      (await this.userModel.findOne({
+        username: loginDto.usernameOrEmail,
+      })) ||
+      (await this.userModel.findOne({ email: loginDto.usernameOrEmail }));
+    if (!user) {
+      throw new HttpException(
+        'username or password is wrong',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    const isCorrectPassword = await comparePassword(
+      loginDto.password,
+      user.password,
+    );
+    if (!isCorrectPassword) {
+      throw new HttpException(
+        'username or password is wrong',
+        HttpStatus.UNPROCESSABLE_ENTITY,
+      );
+    }
+    return user;
   }
 }
